98d60b950d5ed4856ab29360658d0aa5b7d34dd3
Hayden Barnett <haydenbarnett93@gmail.com>
2024-05-03 17:12:53 -0500

    updated dotenv config

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,6 @@
 import { Bot } from "grammy";
 import Groq from "groq-sdk";
-import dotenv from 'dotenv'
-
-dotenv.config();
+import 'dotenv/config';
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
